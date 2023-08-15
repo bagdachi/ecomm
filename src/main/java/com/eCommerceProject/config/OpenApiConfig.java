678fe6fc4b6362c81ff47ffba663ce5a63651f97
Chirag <chiragbagda@gmail.com>
2023-08-15 15:37:20 +0530

    Init

---
+++ src/main/java/com/eCommerceProject/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.eCommerceProject.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//
+//@OpenAPIDefinition(info = @Info(
+//        @Contact(
+//                name = "Chirags Market Api",
+//
+//
+//        ),
+//
+//))
+
+public class OpenApiConfig {
+
+
+}
